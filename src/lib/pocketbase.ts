1a5e18e1d35f7c962966575367bba4bec34bd174
stephenswetonic <sswetonc@uw.edu>
2023-03-20 14:05:28 -0700

    env add

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,6 +1,7 @@
 import PocketBase from 'pocketbase';
 import { writable } from 'svelte/store';
+import { PUBLIC_POCKETBASE_URL } from '$env/static/public';
 
-export const pb = new PocketBase('http://127.0.0.1:8090');
+export const pb = new PocketBase(PUBLIC_POCKETBASE_URL);
 
 export const currentUser = writable(pb.authStore.model);
