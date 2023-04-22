745f9f88403e653fbb4b0bc47dcdec0cd4b44844
stephenswetonic <sswetonc@uw.edu>
2023-04-22 14:49:39 -0700

    style changes

--- src/lib/components/InfiniteCollage.svelte
+++ src/lib/components/InfiniteCollage.svelte
@@ -2,6 +2,7 @@
     import movieIds from "$lib/top250ids.json";
     import Lazy from "svelte-lazy"
     import { onMount } from "svelte";
+    import { fade } from 'svelte/transition';
 
 	let divElement;
     let movieIndex = 0;
@@ -15,14 +16,16 @@
 	async function loadMore() {
         let stop = movieIndex + 10;
 		for (let i = 0; movieIndex < stop; movieIndex++) {
-            const url = "http://img.omdbapi.com/?i=" + movieIds[movieIndex].id + '&h=500' + '&apikey=5db6accd';
-            try {
-                const response = await fetch(url);
+            const url = "http://img.omdbapi.com/?i=" + movieIds[movieIndex].id + '&h=400' + '&apikey=5db6accd';
+            const response = await fetch(url);
+            const posterResponse = await fetch(response.url);
+            if (posterResponse.status != 404) {
                 items.push(response.url);
-                items = items; //must be done for svelte reactivity
-            } catch(err) {
-                console.log(err);
+            items = items; //must be done for svelte reactivity
+            
             }
+
+            
             
 		}
 	}
@@ -75,11 +78,11 @@
 
 
 <div bind:this={divElement} class="container mx-auto px-5 py-2 lg:px-32 lg:pt-12" id="collage">
-    <div class="-m-1 flex flex-wrap md:-m-2">
+    <div class="-m-1 flex flex-wrap md:-m-2 justify-center">
         {#each items as item}
 
-            <Lazy height=200 fadeOption={{delay: 100, duration: 5000}} class="flex flex-wrap p-1">
-                <img alt="" src={item} class="block h-full w-full rounded-lg object-cover object-center"/>
+            <Lazy fadeOption={{delay: 0, duration: 500}} class="p-1 shrink" >
+                <img alt="" src={item} class="my-auto h-auto w-full rounded-lg" in:fade="{{ duration: 500 }}"/>
             </Lazy>
 
         {/each}
@@ -87,14 +90,21 @@
 </div>
 
 <style>
-    	#collage{
-		/* We need to limit the height and show a scrollbar */
-		height: 800px;
-		overflow: auto;
+    	#collage {
+            /* We need to limit the height and show a scrollbar */
+            height: 800px;
+            overflow: auto;
 
-		/* Optional, only to check that it works with margin/padding */
-		margin: 30px;
-		padding: 20px;
-		border: 10px solid black;
-	}
+            -ms-overflow-style: none;  /* IE and Edge */
+            scrollbar-width: none;  /* Firefox */
+
+            /* Optional, only to check that it works with margin/padding */
+            margin: 30px;
+            padding: 20px;
+            border: 10px solid black;
+	    }
+
+        #collage::-webkit-scrollbar {
+            display: none;
+        }
 </style>
